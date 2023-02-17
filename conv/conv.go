e05f3be8f8e2a534c8f8f7b169ba03c6b71c9307
Brage <bragebigrude@gmail.com>
2023-02-17 12:57:02 +0100

    Fikset formler slik at de gir ut tall med bare to desimaler, fikk også passet de testene som var lagt til

--- conv/conv.go
+++ conv/conv.go
@@ -1,5 +1,6 @@
 package conv
 
+import "math"
 /*
   I denne pakken skal alle konverteringfunksjonene
   implementeres. Bruk engelsk.
@@ -11,25 +12,31 @@ package conv
 
 // Konverterer Farhenheit til Celsius
 func FarhenheitToCelsius(farhenheit float64) float64 {
-	return ((farhenheit - 32)*(5/9))
+	celsiusF := (farhenheit - 32)*(5.0/9.0)
+  return (math.Round(celsiusF*100)/100)
 }
 
 func FarhenheitToKelvin(farhenheit float64) float64 {
-  return ((farhenheit - 32) * (5/9) + 273.15)
+  kelvinF := (farhenheit - 32) * (5.0/9.0) + 273.15
+  return (math.Round(kelvinF*100)/100)
 }
 
-func CelsiusToFahrenheit(celsius float64) float64 {
-  return (celsius*(9/5) + 32)
+func CelsiusToFarhenheit(celsius float64) float64 {
+  farhenheitC := (celsius*(9.0/5.0)) + 32
+  return (math.Round(farhenheitC*100)/100)
 }
 
 func CelsiusToKelvin(celsius float64) float64 {
-  return (celsius + 273.15)
+  kelvinC := (celsius + 273.15)
+  return (math.Round(kelvinC*100)/100)
 }
 
 func KelvinToCelsius(kelvin float64) float64 {
-  return (kelvin - 273.15)
+  celsiusK := (kelvin - 273.15)
+  return (math.Round(celsiusK*100)/100)
 }
 
 func KelvinToFarhenheit(kelvin float64) float64 {
-  return ((kelvin - 273.15)*(9/5) + 32)
+  farhenheitK := (kelvin - 273.15)*(9.0/5.0) + 32
+  return (math.Round(farhenheitK*100)/100)
 }
--- conv/conv_test.go
+++ conv/conv_test.go
@@ -21,16 +21,103 @@ func TestFarhenheitToCelsius(t *testing.T) {
 	}
 
 	tests := []test{
-		{input: 134, want: 56.67},
+		{input: 32, want: 0},
 	}
 
-	for _, tc := range tests {
-		got := FarhenheitToCelsius(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
-			t.Errorf("expected: %v, got: %v", tc.want, got)
+	for _, tfc := range tests {
+		got := FarhenheitToCelsius(tfc.input)
+		if !reflect.DeepEqual(tfc.want, got) {
+			t.Errorf("expected: %v, got: %v", tfc.want, got)
 		}
 	}
 }
 
-// De andre testfunksjonene implementeres her
-// ...
+func TestFarhenheitToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+	
+	tests := []test{
+		{input: 134.01, want: 329.82},
+	}
+
+	for _, tfk := range tests {
+		got := FarhenheitToKelvin(tfk.input)
+		if !reflect.DeepEqual(tfk.want, got) {
+			t.Errorf("expected: %v, got: %v", tfk.want, got)
+		}	
+	}
+}
+
+func TestCelsiusToFarhenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+	
+	tests := []test{
+		{input: -89.4, want: -128.92},
+	}
+
+	for _, tcf := range tests {
+		got := CelsiusToFarhenheit(tcf.input)
+		if !reflect.DeepEqual(tcf.want, got) {
+			t.Errorf("expected: %v, got: %v", tcf.want, got)
+		}	
+	}
+}
+
+func TestCelsiusToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+	
+	tests := []test{
+		{input: -89.4, want: 183.75}, //skal egentlig være + på want ikke - som Janis sier
+	}
+
+	for _, tck := range tests {
+		got := CelsiusToKelvin(tck.input)
+		if !reflect.DeepEqual(tck.want, got) {
+			t.Errorf("expected: %v, got: %v", tck.want, got)
+		}	
+	}
+}
+
+func TestKelvinToCelsius(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+	
+	tests := []test{
+		{input: 273.15, want: 0},
+	}
+
+	for _, tkc := range tests {
+		got := KelvinToCelsius(tkc.input)
+		if !reflect.DeepEqual(tkc.want, got) {
+			t.Errorf("expected: %v, got: %v", tkc.want, got)
+		}	
+	}
+}
+
+func TestKelvinToFarhenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+	
+	tests := []test{
+		{input: 329.82, want: 134.01},
+	}
+
+	for _, tkf := range tests {
+		got := KelvinToFarhenheit(tkf.input)
+		if !reflect.DeepEqual(tkf.want, got) {
+			t.Errorf("expected: %v, got: %v", tkf.want, got)
+		}	
+	}
+}
